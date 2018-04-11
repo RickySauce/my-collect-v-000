26834abebc320c4910b76471ec60992e291bb4f3
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-11 13:28:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    collection = Array.new
+    while i < array.length
+        collection << yield array[i]
+        i += 0
+      else
+        false
+      end
+    end
+  end
+  collection
+end
